d054cb39910d57eec9a51c81f7c2ec63d4e9d393
Mark Rivers <rivers@cars.uchicago.edu>
2015-03-29 15:48:43 -0500

    Changes for ADCore R2-2

--- iocs/andor3IOC/iocBoot/iocAndor3/st.cmd
+++ iocs/andor3IOC/iocBoot/iocAndor3/st.cmd
@@ -4,30 +4,38 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/andor3App.dbd")
 andor3App_registerRecordDeviceDriver(pdbbase) 
 
+# Prefix for all records
 epicsEnvSet("PREFIX", "13ANDOR3:")
+# The port name for the detector
 epicsEnvSet("PORT",   "ANDOR")
+# The camera number in the system
 epicsEnvSet("CAMERA", "0")
+# The queue size for all plugins
 epicsEnvSet("QSIZE",  "21")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "2592")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "2160")
+# The maximum number of time seried points in the NDPluginStats plugin
 epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # andor3Config(const char *portName, int cameraId, int maxBuffers,
 #              size_t maxMemory, int priority, int stackSize,
 #              int maxFrames)
 andor3Config("$(PORT)", $(CAMERA), 0, 0, 0, 0, 100)
-dbLoadRecords("$(ADCORE)/db/ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-#dbLoadRecords("$(ADCORE)/db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADANDOR3)/db/andor3.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADANDOR3)/db/andor3.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
-dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 # Make NELEMENTS in the following be a little bigger than 2048*2048
 # Use the following command for 32-bit images.  This is needed for 32-bit detectors or for 16-bit detectors in acccumulate mode if it would overflow 16 bits
-#dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int32,FTVL=LONG,NELEMENTS=5600000")
+#dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=5600000")
 # Use the following command for 16-bit images.  This can be used for 16-bit detector as long as accumulate mode would not result in 16-bit overflow
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=5600000")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=5600000")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
